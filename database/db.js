ba4070e1bbe3aeda58fb6ca955e07c3caa18ea96
felipe <pipemendezri@gmail.com>
2025-06-07 22:48:03 -0400

    Update db.js

--- database/db.js
+++ database/db.js
@@ -4,3 +4,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Conectado a MongoDB'))
     .catch(err => console.error('Error de conexión:', err));
+
+module.exports = mongoose;
+
+//Para usarlo en otros archivos/servicios:
+//const mongoose = require('./ruta/al/archivo');
